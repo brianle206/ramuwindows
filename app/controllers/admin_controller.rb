81f8e3945e18d4072cfda78d9231419d11ec74ba
Brian Le <brianle206@gmail.com>
2016-08-22 13:17:22 -0700

    Dashboard work

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -10,7 +10,26 @@ class AdminController < ApplicationController
 		end
 	end
 	def show
+		@users = User.all.order('id ASC')
+		render 'manage'
+	end
+	def manage
 		
 	end
 
+	def make_admin
+		user = User.find(params[:id])
+		user.update(admin: true)
+		redirect_to root_path
+	end
+	def unmake_admin
+		user = User.find(params[:id])
+		user.update(admin: false)
+		redirect_to root_path
+	end
+	def destroy
+		User.find(params[:id]).destroy
+		redirect_to '/admin/manage'
+	end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
 
   devise_for :users
   resources :articles, :lecture, :admin
+  get '/destroy/:id' => 'admin#destroy'
+  get '/make_admin/:id' => 'admin#make_admin'
+  get '/unmake_admin/:id' => 'admin#unmake_admin'
   root 'articles#landing'
   get '/search' => 'articles#search'
   get '/new' => 'articles#new'
@@ -15,5 +18,6 @@ Rails.application.routes.draw do
   get '/articles/:id/lecture/:lid/edit' => 'articles#lecture_edit'
   post '/articles/:id/create_lecture' => 'articles#create_lecture'
   get '/admin' => 'admin#index'
+  get '/admin/manage' => 'admin#manage'
   delete '/articles/:id/lecture/:lid' => 'articles#lecture_destroy'
 end
