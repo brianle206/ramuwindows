7933e8386bf6b23fcf7607ee2c40bbd7a8b52b63
Brian Le <brianle206@gmail.com>
2016-10-17 09:49:29 -0700

    shows correct answers

--- app/helpers/quiz_helper.rb
+++ app/helpers/quiz_helper.rb
@@ -7,15 +7,13 @@ module QuizHelper
 	def check_user_answers(quiz)
 		@quiz = Quiz.find(quiz)
 		@userAnswer = UserQuizAnswer.where(quiz_id: quiz, user_id: current_user.id)
-		@questions = @quiz.questions.count
+		@questions = @quiz.questions.count		
+		@questionAnswer = @quiz.questions
 		@correct = 0
-		
 	end
 
-
 	def answer_content(answer)
-		@answer = Answer.find(answer)
-		@answerContent = @answer.content
+		@answer = Answer.find(answer).content
 	end
 
 	def question_content(question)
