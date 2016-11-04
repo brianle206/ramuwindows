a7add185135950dbaf510fb968382e7ef571ad73
Brian Le <brianle206@gmail.com>
2016-11-04 08:59:33 -0700

    video trails

--- app/assets/javascripts/plugins/index.js
+++ app/assets/javascripts/plugins/index.js
@@ -1,6 +1,6 @@
 $('document').ready(function() {
-	var options = { videoId: 'F3vRd30WH5Q', start: 3 };
-	$('#wrapper').tubular(options);
+	var options = { videoId: 'F3vRd30WH5Q', start: 3, mute: false,volumeUpClass: 'tubular-volume-up' };
+	$('#video').tubular(options);
 	// f-UGhWj1xww cool sepia hd
 	// 49SKbS7Xwf4 beautiful barn sepia
 });
